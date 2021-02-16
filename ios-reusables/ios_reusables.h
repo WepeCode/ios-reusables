234784c81b2becbf47c16fd2eba6181238650887
Vision Mkhabela <vision.mkhabela@standardbank.co.za>
2021-02-16 14:02:20 +0200

    Initial Commit

---
+++ ios-reusables/ios_reusables.h
@@ -0,0 +1,18 @@
+//
+//  ios_reusables.h
+//  ios-reusables
+//
+//  Created by Vision Mkhabela on 2021/02/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_reusables.
+FOUNDATION_EXPORT double ios_reusablesVersionNumber;
+
+//! Project version string for ios_reusables.
+FOUNDATION_EXPORT const unsigned char ios_reusablesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_reusables/PublicHeader.h>
+
+
---
+++ ios-reusablesTests/ios_reusablesTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_reusablesTests.swift
+//  ios-reusablesTests
+//
+//  Created by Vision Mkhabela on 2021/02/16.
+//
+
+import XCTest
+@testable import ios_reusables
+
+class ios_reusablesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
